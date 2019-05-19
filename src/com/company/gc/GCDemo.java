3704a16708159fd16f1769c5ed5adc83800f9c8f
a.zykov <18june85@gmail.com>
2019-05-19 23:02:25 +0300

    change GC demo

--- src/com/company/gc/GCDemo.java
+++ src/com/company/gc/GCDemo.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+
 /**
  * Для использования Flight Recorder в JMC добавьте флаги:
  * -XX:+UnlockCommercialFeatures -XX:+FlightRecorder
+ * Пример 1: -XX:+UseSerialGC -Xmx100m
+ * Пример 2: -XX:+UseParallelGC -Xmx100m
+ * Пример 3: -XX:+UseConcMarkSweepGC -Xmx100m
+ * Пример 3: -XX:+UseG1GC -Xmx100m
  */
 public class GCDemo {
     private static final int LOOP_COUNT = 100_000_000;
@@ -18,7 +23,10 @@ public class GCDemo {
         for (int i = 0; i < LOOP_COUNT; i++) {
             String str = "" + random.nextInt();
             list.add(str);
-
+            if (i % 10 == 0) {
+                Thread.sleep(1);
+                list.remove(0);
+            }
         }
         System.out.println(list.size());
 
